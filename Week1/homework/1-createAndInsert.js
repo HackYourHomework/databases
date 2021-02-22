fd80b6b0d198ab7905a227adfceb57abf3ee3254
MoSaif00 <moh14ab@gmail.com>
2021-02-22 23:36:01 +0100

    updated first exercise and added data file

--- Week1/homework/1-createAndInsert.js
+++ Week1/homework/1-createAndInsert.js
@@ -1,74 +1,61 @@
+const data = require('./1-data');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
 });
-
 connection.connect(function (err) {
   if (err) throw err;
   console.log('Node.js connected to MySQl!');
 });
 
-// function to execute sql qury
+// function to execute sql query and other function to insert data to tables
 function sqlQuery(queryString, message) {
   connection.query(queryString, function (err, result) {
     if (err) throw err;
     console.log(message);
   });
 }
+function insertRow(queryString, data) {
+  connection.query(queryString, [data], function (err, results) {
+    if (err) throw err;
+    console.log('Data Inserted');
+  });
+}
 
 // Create a database called meetup
-sqlQuery('CREATE DATABASE test5', 'Database created');
-
+sqlQuery('CREATE DATABASE meetup', 'Database created');
 // use the created database
-sqlQuery('use test5', 'we are in the database');
-
+sqlQuery('use meetup', 'we are in the database');
 // Create a table called Invitee with the following fields (invitee_no, invitee_name and invited_by).
 sqlQuery(
   'CREATE TABLE Invitee (invitee_no INT(10) AUTO_INCREMENT, invitee_name VARCHAR(50), invited_by VARCHAR(50), PRIMARY KEY(invitee_no))',
   'table created'
 );
-
 //Create a table called Room with the following fields (room_no, room_name and floor_number)
 sqlQuery(
   'CREATE TABLE Room (room_no INT(5) AUTO_INCREMENT, room_name VARCHAR(20),floor_number INT(10),PRIMARY KEY(room_no))',
   'table created'
 );
-
 // Create a table called Meeting with the following fields (meeting_no, meeting_title, starting_time, ending_time,room_no)
 sqlQuery(
   'CREATE TABLE Meeting (meeting_no INT(5) AUTO_INCREMENT, meeting_title VARCHAR(30), starting_time DATETIME, ending_time DATETIME,room_no INT(10),PRIMARY KEY(meeting_no))',
   'table created'
 );
 
-// // Insert 5 rows into each table with relevant fields. Find a way to create the data for those fields
-// for (let i = 1; i < 6; i++) {
-//   connection.query(
-//     `INSERT INTO Invitee( invitee_name, invited_by )VALUES ( 'Invitee name ',  'Name')`,
-//     function (err, result) {
-//       if (err) throw err;
-//       console.log('Data Inserted');
-//     }
-//   );
-//   connection.query(
-//     `INSERT INTO Room (room_name, floor_number ) VALUES( 'room name ${i}', ${
-//       i + 2
-//     })`,
-//     function (err, result) {
-//       if (err) throw err;
-//       console.log('Data Inserted');
-//     }
-//   );
-//   connection.query(
-//     `INSERT INTO Meeting ( meeting_title, starting_time, ending_time, room_no)VALUES( 'meeting title ${i}','2021-03-01 1${i}:00:00', '2021-03-01 1${
-//       i + 1
-//     }:00:00',  ${i})`,
-//     function (err, result) {
-//       if (err) throw err;
-//       console.log('Data Inserted');
-//     }
-//   );
-// }
+// Insert 5 rows into each table with relevant fields. Find a way to create the data for those fields
+insertRow(
+  'INSERT INTO Invitee( invitee_name, invited_by )VALUES ?',
+  data.inviteeData
+);
+insertRow(
+  'INSERT INTO Room (room_name, floor_number ) VALUES ?',
+  data.roomsData
+);
+insertRow(
+  'INSERT INTO Meeting ( meeting_title, starting_time, ending_time, room_no)VALUES ?',
+  data.meetingData
+);
 
 connection.end();
---
+++ Week1/homework/1-data.js
@@ -0,0 +1,25 @@
+const inviteeData = [
+  ['Mo', 'Salem'],
+  ['Tong', 'Ahmed'],
+  ['Wouter', 'Tomas'],
+  ['Salem', 'Saif'],
+  ['Ekmel', 'Saif'],
+];
+const roomsData = [
+  ['first Room', '05'],
+  ['second Room', '05'],
+  ['Third Room', '07'],
+  ['Fourth Room', '02'],
+  ['Fifth Room', '01'],
+];
+const meetingData = [
+  ['first meeting', '2021-03-01 10:00:00', '2021-03-01 12:00:00', 2],
+  ['Second meeting', '2021-03-01 11:00:00', '2021-03-01 12:00:00', 5],
+  ['Third meeting', '2021-03-01 12:00:00', '2021-03-01 14:00:00', 1],
+  ['Fourth meeting', '2021-03-01 12:00:00', '2021-03-01 13:00:00', 3],
+  ['Fifth meeting', '2021-03-01 11:00:00', '2021-03-01 12:00:00', 4],
+];
+
+module.exports.inviteeData = inviteeData;
+module.exports.roomsData = roomsData;
+module.exports.meetingData = meetingData;
