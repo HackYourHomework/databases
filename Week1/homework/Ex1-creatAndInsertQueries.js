3d5c2b2f61ec170afcafec88c9943281d3787b3f
Khaled Alsaleh <khalsalehsyria1@gmail.com>
2021-02-23 20:29:37 +0100

    Rebuild meetup DB With Every Execution

--- Week1/homework/Ex1-creatAndInsertQueries.js
+++ Week1/homework/Ex1-creatAndInsertQueries.js
@@ -1,4 +1,4 @@
-// First I Created The Database (meetup) Using Mysql Command Line Client!
+// First I Created The Database (meetup) Using Mysql Command Line Client! Ex1 - 1.Create a database called meetup
 // This Is The SQL Query : CREATE DATABASE meetup;
 
 
@@ -13,8 +13,36 @@ const connection = mysql.createConnection({
   port : 3306
 });
 
-// Start Connection With meetup DB!
 connection.connect();
+
+// Drop Database meetup! 
+// we don't have to check if the db is exists,
+// because I know it is exists since I create it using CLI client or workbench
+// And I didn't drop the tables(It will dropped for sure ), cause I drop the DB (the container of the tables)
+const drop_db_meetup = "DROP DATABASE IF EXISTS meetup ";
+connection.query(drop_db_meetup , function (error, results, fields) {
+    if (error) {
+        throw error;
+    }
+    console.log(`Database meetup Dropped!`);
+});
+
+
+// Create Database meetup!
+//since we drop the db, I don't have to check if it's exists! 
+const create_db_meetup = "CREATE DATABASE meetup";
+connection.query(create_db_meetup, function (error, results, fields) {
+    if (error) {
+        throw error;
+    }
+    console.log(`Database meetup Created!`);
+});
+
+// Use meetup DB
+connection.query(("USE meetup"), (error, results, fields) => {
+    if (error) throw error;
+});
+
 
 // Write Query To Create Each Table With Table Name, Entity Names And Entity Data Type!
 const create_query_invitee = "create table Invitee (invitee_no int, invitee_name varchar(50), invited_by varchar(50))";
@@ -53,7 +81,7 @@ const insert_query_room = [
     "insert into Room values (36, 'Room2', 1)",
     "insert into Room values (11, 'Room4', 2)",
     "insert into Room values (73, 'Room5', 2)",
-    "insert into Room values (22, 'Room6', 3)"
+    "insert into Room values (25, 'Room25', 25)"
     ];
 const insert_query_meeting = [
     "insert into Meeting values (50, 'Title1', '2019-01-01 15:00:00', '2019-01-01 17:00:00', 1)",
