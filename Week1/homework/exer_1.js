f6d42de68077db11b821c4556b5c10ce4c9e076a
Manal <manalhdkz@gmail.com>
2021-02-23 20:47:53 +0100

    ex1 & ex2

---
+++ Week1/homework/exer_1.js
@@ -0,0 +1,83 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'hyfuser',
+  password : 'hyfpassword',
+  port: 3306
+});
+
+connection.connect();
+
+//drop DB mmetup 
+connection.query("DROP DATABASE meetup", (error, results, fields) => {
+    if (error) {
+         throw error;
+     }
+     console.log("DB dropped");
+});
+//create DB meetup
+connection.query("CREATE DATABASE IF NOT EXISTS meetup", (error, results, fields) => {
+    if (error) {
+         throw error;
+     }
+     console.log("database meetup created");
+});
+//connect/use DB
+connection.query(("USE meetup"), (error, results, fields) => {
+    if (error) throw error;
+});
+ 
+
+const createTableInvitee = "create table Invitee (invitee_no int, invitee_name varchar(20), invited_by varchar(20))";
+const createTableRoom = "create table Room (room_no int, room_name varchar(20), floor_number int)";
+const createTableMeeting = "create table Meeting (meeting_no int, meeting_title varchar(10), starting_time datetime, ending_time datetime, room_no int)";
+
+function createTable(createTableQuery){
+    connection.query(createTableQuery, function (error, results, fields) {
+        if (error) {
+            throw error;
+        }
+        console.log("table is created");
+    });
+}
+createTable(createTableInvitee);
+createTable(createTableRoom);
+createTable(createTableMeeting);
+
+//insert data
+const insertTableInvitee = [
+    "INSERT INTO Invitee  values (20, 'mohammed', 'manal')",
+    "INSERT INTO Invitee  values (32, 'hashim', 'ziad')",
+    "INSERT INTO Invitee  values (12, 'tom', 'david')",
+    "INSERT INTO Invitee  values (20, 'khaled', 'manal')",
+    "INSERT INTO Invitee  values (40, 'sami', 'ziad')"
+    ];
+const insertTableRoom = [
+    "INSERT INTO Room values (12, 'A', 0)",
+    "INSERT INTO Room values (20, 'B', 1)",
+    "INSERT INTO Room values (17, 'C', 2)",
+    "INSERT INTO Room values (33, 'D', 3)",
+    "INSERT INTO Room values (11, 'E', 4)"
+    ];
+const insertTableMeeting = [
+    "INSERT INTO Meeting values (20, 'Finance', '2020-02-20 12:00:00', '2020-02-20 02:00:00', 3)",
+    "INSERT INTO Meeting values (10, 'Admin', '2020-01-22 11:00:00', '2020-01-22 01:00:00', 4)",
+    "INSERT INTO Meeting values (12, 'HR', '2020-03-22 15:00:00', '2020-03-22 18:00:00', 1)",
+    "INSERT INTO Meeting values (13, 'IT', '2020-02-14 11:00:00', '2020-02-14 13:00:00', 5)",
+    "INSERT INTO Meeting values (80, 'Logistics', '2020-02-24 13:00:00', '2020-02-24 15:00:00', 1)"
+    ];
+
+function insertData(arrayOfQueries){
+    arrayOfQueries.forEach(element => {
+        connection.query(element, function (error, results, fields) {
+            if (error) {
+                throw error;
+            }
+            console.log("Data inserted");
+        });
+    });
+};
+insertData(insertTableInvitee);
+insertData(insertTableRoom);
+insertData(insertTableMeeting);
+connection.end();
---
+++ Week1/homework/exer_2.js
@@ -0,0 +1,34 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'hyfuser',
+  password : 'hyfpassword',
+  database : 'world',
+  port: 3306
+});
+
+connection.connect();
+
+const select_query = [
+"SELECT name FROM country WHERE population > 8000000", 
+"SELECT name FROM country WHERE name LIKE '%land%'",
+"SELECT name FROM city WHERE population between 50000 and 1000000",
+"SELECT name FROM country WHERE name like 'EUROPE'",
+"SELECT name, SurfaceArea FROM country ORDER BY SurfaceArea DESC",
+"SELECT name FROM city WHERE CountryCode = 'NLD'",
+"SELECT population FROM city WHERE name = 'Rotterdam'",
+"SELECT name, population FROM city ORDER BY population DESC LIMIT 10",
+"SELECT name FROM city ORDER BY population DESC LIMIT 10",
+"SELECT SUM(Population) FROM country"
+];
+
+for(let i in select_query){
+    connection.query(select_query[i],(error, results, fields) => {
+        if (error) {
+            throw error;
+        }
+        console.log("the result is ", results);
+    });
+}
+
+connection.end();
