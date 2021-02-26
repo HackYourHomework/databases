d2837656d8e56740d029d215612821fdb3a1b4aa
Ahmad Magdy <amagsid@gmail.com>
2021-02-26 19:07:34 +0100

    added research paper data

--- Week2/data/research-papers.js
+++ Week2/data/research-papers.js
@@ -1,85 +1,89 @@
 const researchPapers = [
   {
     paper_id: 1001,
-    paper_title: "Ronald C Kessler",
+    paper_title:
+      "An Analysis of the Faith in Human Progress in the Age of Enlightenment",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1002,
-    paper_title: "Ronald C Kessler",
+    paper_title:
+      "Factors Contributing to Violent and Unremorseful Behaviors in Teens",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1003,
-    paper_title: "Ronald C Kessler",
+    paper_title: "The Several Reasons Why People Should Live More Freely",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1004,
-    paper_title: "Ronald C Kessler",
+    paper_title: "The Six Steps of the Scientific Method",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1005,
-    paper_title: "Ronald C Kessler",
+    paper_title:
+      "The History and Factors That Influenced England's Break from Rome",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1006,
-    paper_title: "Ronald C Kessler",
+    paper_title: "An Overview on Data Transfer in Computer Network",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1007,
-    paper_title: "Ronald C Kessler",
+    paper_title: "The Meaning and Significance of the Word Feminist",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1008,
-    paper_title: "Ronald C Kessler",
+    paper_title: "Civil Rights Movement Rediscovered",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1009,
-    paper_title: "Ronald C Kessler",
+    paper_title:
+      "The Issues Surrounding Editorial Departments in Daily Press in Canada",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1010,
-    paper_title: "Ronald C Kessler",
+    paper_title: "The Advancements of Technology in Films Over the Years",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1011,
-    paper_title: "Ronald C Kessler",
+    paper_title: "An Analysis of the Nature of Sharks",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1012,
-    paper_title: "Ronald C Kessler",
+    paper_title: "A Study of the Ozone",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1013,
-    paper_title: "Ronald C Kessler",
+    paper_title: "An Analysis of Ebonics",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1014,
-    paper_title: "Ronald C Kessler",
+    paper_title: "A Definition and Uses of 3D Model Printing",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
@@ -97,7 +101,8 @@ const researchPapers = [
   },
   {
     paper_id: 1017,
-    paper_title: "Ronald C Kessler",
+    paper_title:
+      "Creationist View of the Origin of Life and Process of Evolution",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
@@ -115,7 +120,7 @@ const researchPapers = [
   },
   {
     paper_id: 1020,
-    paper_title: "Ronald C Kessler",
+    paper_title: "An Introduction to the Life and Work by Karl Marxr",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
@@ -133,19 +138,19 @@ const researchPapers = [
   },
   {
     paper_id: 1023,
-    paper_title: "Ronald C Kessler",
+    paper_title: "An Analysis of Autonomy and benevolent Lies by Thomas Hill",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1024,
-    paper_title: "Ronald C Kessler",
+    paper_title: "The Factors That Impact on Malaysian's Economy",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1025,
-    paper_title: "Ronald C Kessler",
+    paper_title: "The Problem of Increasing Population Growth",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
@@ -157,25 +162,26 @@ const researchPapers = [
   },
   {
     paper_id: 1027,
-    paper_title: "Ronald C Kessler",
+    paper_title: "The Importance of Integrity in the Workplace",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1028,
-    paper_title: "Ronald C Kessler",
+    paper_title:
+      "The Effects of the High-Income Nations' Industrialization in the United States",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1029,
-    paper_title: "Ronald C Kessler",
+    paper_title: "An Examination of Descartes's Views on Dreams",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
   {
     paper_id: 1030,
-    paper_title: "Ronald C Kessler",
+    paper_title: "Computerizing Business Accounts",
     conference: "Harvard University",
     publish_date: "1960-05-12",
   },
