4b51c57869f5ad0cd1d129d576d8f855bf438fd5
Khaled Alsaleh <khalsalehsyria1@gmail.com>
2021-02-27 22:08:37 +0100

    Edit Ex1, Ex2 & Insert Data

--- Week2/homework/ex1-keys.js
+++ Week2/homework/ex1-keys.js
@@ -1,46 +1,46 @@
+
+// I Start This Week's Homework By Creating week2 Database On Workbench using SQL Query: create database week2;
+
 const util = require('util');
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+
+const CONNECTION_CONFIG = {
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
   database: 'week2',
   port: 3306
-});
+};
 
-// Promisify the bind function of query function of connection object
-// Pass connection object (because bind expects "this")
-// Afterwards execQuery will work as a function that returns a promise but
-// we don't have to call "then" over that promise
-const execQuery = util.promisify(connection.query.bind(connection));
+const CREATE_AUTHORS_TABLE = `
+CREATE TABLE IF NOT EXISTS authors (
+    author_no INT auto_increment PRIMARY KEY,
+    author_name VARCHAR(100),
+    university VARCHAR(100),
+    date_of_birth DATE,
+    h_index int,
+    gender ENUM('m', 'f')
+);`;
+
+const ALTER_AUTHORS_TABLE = `ALTER TABLE authors add column mentor int,
+                                add constraint fk_mentor foreign key (mentor)
+                                references authors(author_no);`;
+
+
 
 async function seedDatabase() {
-  const CREATE_AUTHORS_TABLE = `
-    CREATE TABLE IF NOT EXISTS authors (
-        author_no INT auto_increment PRIMARY KEY ,
-        author_name VARCHAR(50),
-        university VARCHAR(50),
-        date_of_birth DATE,
-        h_index int,
-        gender ENUM('m', 'f')
-    );`;
-    const ALTER_AUTHORS_TABLE = `
-        ALTER TABLE authors add column mentor int,
-        add constraint fk_author foreign key (mentor) references authors(author_no);`;
-  
-  connection.connect();
-
+  const connection = mysql.createConnection(CONNECTION_CONFIG);
+  const execQuery = util.promisify(connection.query.bind(connection));
   try {
     // call the function that returns promise
     await execQuery(CREATE_AUTHORS_TABLE);
     await execQuery(ALTER_AUTHORS_TABLE);
-  } catch (error) {
-    console.error(error);
     connection.end();
-  }
-
-  connection.end();
+  } catch (error) {
+    console.error(error.message);
+    connection.end();
+  } 
 }
 
 seedDatabase();
---
+++ Week2/homework/ex3-joins.js
@@ -0,0 +1,5 @@
+// select E1.author_name As Authors, E2.author_name as Mentors
+// from authors as E1 inner join authors as E2 
+// on E1.mentor = E2.author_no;
+
+
