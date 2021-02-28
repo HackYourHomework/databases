f3ed026a30d464dc4bf85430f1601709d1af85d4
MoSaif00 <moh14ab@gmail.com>
2021-02-28 21:56:31 +0100

    test data

--- Week2/homework/data.js
+++ Week2/homework/data.js
@@ -7,13 +7,13 @@ for (i = 0; i < 15; i++) {
 
   authorName.push(firstNameRandom + ' ' + lastNameRandom);
 }
-console.log(authorName);
+
 const universityName = [];
 for (i = 0; i < 15; i++) {
   const randomUniversity = faker.address.country();
   universityName.push(randomUniversity + ' university');
 }
-console.log(universityName);
+
 const dateOfBirth = [];
 for (i = 0; i < 15; i++) {
   function randomDate(start, end) {
@@ -25,13 +25,13 @@ for (i = 0; i < 15; i++) {
   }
   dateOfBirth.push(randomDate(new Date(1950, 0, 1), new Date(2000, 0, 1)));
 }
-console.log(dateOfBirth);
+
 const hIndex = [];
 for (i = 0; i < 15; i++) {
   const hIndexRandom = faker.random.float();
   hIndex.push(hIndexRandom);
 }
-console.log(hIndex);
+
 const gender = [];
 for (i = 0; i < 15; i++) {
   const maleOrFemale = ['m', 'f'];
@@ -39,20 +39,27 @@ for (i = 0; i < 15; i++) {
   const genderRandom = faker.random.arrayElement(maleOrFemale);
   gender.push(genderRandom);
 }
-console.log(gender);
+
+const mentorNo = [];
+for (i = 0; i < 15; i++) {
+  while (mentorNo.length < 15) {
+    let randomNumber = Math.floor(Math.random() * 14) + 1;
+    if (mentorNo.indexOf(randomNumber) === -1) mentorNo.push(randomNumber);
+  }
+}
 
 const paperTitle = [];
 for (i = 0; i < 15; i++) {
   const paperTitleRandom = faker.name.title();
   paperTitle.push(paperTitleRandom + ' Research');
 }
-console.log(paperTitle);
+
 const conferenceName = [];
 for (i = 0; i < 15; i++) {
   const conferenceRandom = faker.name.title();
   conferenceName.push(conferenceRandom + ' Conference');
 }
-console.log(conferenceName);
+
 const publishDate = [];
 for (i = 0; i < 15; i++) {
   function randomDate(start, end) {
@@ -64,12 +71,14 @@ for (i = 0; i < 15; i++) {
   }
   publishDate.push(randomDate(new Date(2010, 0, 1), new Date(2021, 0, 1)));
 }
-console.log(publishDate);
-const mentorNo = [];
-for (i = 0; i < 15; i++) {
-  while (mentorNo.length < 15) {
-    let randomNumber = Math.floor(Math.random() * 14) + 1;
-    if (mentorNo.indexOf(randomNumber) === -1) mentorNo.push(randomNumber);
-  }
-}
-console.log(mentorNo);
+
+module.exports.authorName = authorName;
+module.exports.universityName = universityName;
+module.exports.dateOfBirth = dateOfBirth;
+module.exports.hIndex = hIndex;
+module.exports.gender = gender;
+module.exports.mentorNo = mentorNo;
+
+module.exports.paperTitle = paperTitle;
+module.exports.conferenceName = conferenceName;
+module.exports.publishDate = publishDate;
