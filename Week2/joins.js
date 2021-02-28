b31e594613eeabf6ff0c59b7c9097f9194726963
Ahmad Magdy <amagsid@gmail.com>
2021-02-28 08:55:20 +0100

    started working on exercise 3.joins

--- Week2/joins.js
+++ Week2/joins.js
@@ -1,1 +1,17 @@
 "use strict";
+
+//NPM pacjages
+const util = require("util");
+const mysql = require("mysql");
+
+//SELECT a.author_name as author, m.author_name as mentor FROM authors m inner join authors a ON a.author_no = m.mentor
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "academy",
+  port: 3306,
+});
+
+const execQuery = util.promisify(connection.query.bind(connection));
