746873e25b227066273d6a18fe9851750ddbfa1c
MoSaif00 <moh14ab@gmail.com>
2021-03-01 00:47:29 +0100

    finished exercis no 2 and inserted necessary data

--- Week2/homework/2-Relationship.js
+++ Week2/homework/2-Relationship.js
@@ -1,12 +1,19 @@
 const data = require('./data');
 const util = require('util');
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
   database: 'userdb',
 });
+
+// function to execute insert queries
+function executeQuery(queryString, data) {
+  const execQuery = util.promisify(connection.query.bind(connection));
+  execQuery(queryString, [data]);
+}
 
 const CREATE_TABLE_RESEARCH_PAPERS = `
 CREATE TABLE IF NOT EXISTS research_papers(
@@ -21,25 +28,12 @@ CREATE TABLE IF NOT EXISTS research_papers(
 const CREATE_TABLE_AUTHOR_RESEARCH_RELATIONSHIP = `
 CREATE TABLE IF NOT EXISTS author_research(
    author_no INT NOT NULL,
-   research_id INT NOT NULL,
+   research_id INT NOT NULL ,
    CONSTRAINT fk_author FOREIGN KEY (author_no) REFERENCES authors(author_no),
-   CONSTRAINT fk_research FOREIGN KEY (research_id) REFERENCES research_papers(paper_id),
-   PRIMARY KEY (author_no, research_id)
+   CONSTRAINT fk_research FOREIGN KEY (research_id) REFERENCES research_papers(paper_id)
 );
 `;
 
-const INSERT_INTO_AUTHORS_TABLE = `
-INSERT INTO authors (author_name, university, date_of_birth, h_index, gender, mentor)
-VALUES (${data.authorName}, ${data.universityName}, ${data.dateOfBirth}, ${data.hIndex}, ${data.gender}, ${data.mentorNo});
-`;
-// const INSERT_INTO_RESEARCH_TABLE = `
-// INSERT INTO research_papers (paper_title, conference, publish_data)
-// VALUES (${data.paperTitle}, ${data.conferenceName}, ${data.publishDate});
-// `;
-// const INSERT_INTO_AUTHORS_AND_RESEARCH_TABLE = `
-// INSERT INTO author_research (author_no, research_id)
-// VALUES (${data.authorNo}, ${data.researchId});
-// `;
 async function seedDatabase() {
   const execQuery = util.promisify(connection.query.bind(connection));
 
@@ -47,8 +41,24 @@ async function seedDatabase() {
     await execQuery(CREATE_TABLE_RESEARCH_PAPERS);
     await execQuery(CREATE_TABLE_AUTHOR_RESEARCH_RELATIONSHIP);
 
-    // await execQuery(INSERT_INTO_RESEARCH_TABLE);
-    // await execQuery(INSERT_INTO_AUTHORS_AND_RESEARCH_TABLE);
+    execQuery('SET FOREIGN_KEY_CHECKS=0');
+    await executeQuery(
+      `INSERT INTO authors (author_name, university, date_of_birth, h_index, gender, mentor) VALUES ?`,
+      data.authorDetails
+    );
+    execQuery('SET FOREIGN_KEY_CHECKS=1');
+
+    await executeQuery(
+      `INSERT INTO research_papers (paper_title, conference, publish_date) VALUES ? `,
+      data.researchDetails
+    );
+
+    execQuery('SET FOREIGN_KEY_CHECKS=0');
+    await executeQuery(
+      `INSERT INTO author_research (author_no,research_id) VALUES ? `,
+      data.authorResearchRelation
+    );
+    execQuery('SET FOREIGN_KEY_CHECKS=1');
   } catch (err) {
     console.error(err.message);
     connection.end();
