c3cc77830be3b76ec84fac311ad5fc20fed9005a
Khaled Alsaleh <khalsalehsyria1@gmail.com>
2021-03-01 01:25:27 +0100

    Ex3, Ex4, ERD

--- Week2/homework/ex3-joins.js
+++ Week2/homework/ex3-joins.js
@@ -1,10 +1,47 @@
-// select E1.author_name As Authors, E2.author_name as Mentors
-// from authors as E1 left join authors as E2 
-// on E1.mentor = E2.author_no;
+const util = require('util');
+const mysql = require('mysql');
 
 
+const CONNECTION_CONFIG = {
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  database: 'week2',
+  port: 3306
+};
 
-// select authors.*, research_papers.paper_title
-// from authors
-// left join authors_research_papers on authors.author_no = authors_research_papers.author_no
-// left join research_papers on authors_research_papers.research_paper_no = research_papers.paper_id
+
+const SELECT_AUTHORS_MENTORS = `
+    SELECT A1.author_name AS authors, A2.author_name AS mentors
+    FROM authors AS A1 LEFT JOIN authors AS A2 
+    ON A1.mentor = A2.author_no;
+    `;
+
+const SELECT_AUTHORS_PAPERS = `
+    SELECT authors.*, research_papers.paper_title
+    FROM authors
+    LEFT JOIN authors_research_papers ON authors.author_no = authors_research_papers.author_no
+    LEFT JOIN research_papers ON authors_research_papers.research_paper_no = research_papers.paper_id;
+    `;
+
+
+async function seedDatabase() {
+  const connection = mysql.createConnection(CONNECTION_CONFIG);
+  const execQuery = util.promisify(connection.query.bind(connection));
+  try {
+    // call the function that returns promise
+
+    console.log("Names Of All Authors And Their Corresponding Mentors:");
+    console.log(await execQuery(SELECT_AUTHORS_MENTORS));
+
+    console.log("Authors And Their Published Paper Title:");
+    console.log(await execQuery(SELECT_AUTHORS_PAPERS));
+
+    connection.end();
+  } catch (error) {
+    console.error(error.message);
+    connection.end();
+  } 
+}
+
+seedDatabase();
--- Week2/homework/ex4-aggregate.js
+++ Week2/homework/ex4-aggregate.js
@@ -1,33 +1,79 @@
-// 1
-// select  research_papers.paper_title, count(authors.author_name)
-// from research_papers
-// left join authors_research_papers on research_paper_no = research_papers.paper_id
-// left join authors on authors_research_papers.author_no = authors.author_no
-// group by paper_title;
+const util = require('util');
+const mysql = require('mysql');
 
 
-
-// 2
-// select count(research_papers.paper_title) As FemalesPapers
-// from research_papers
-// left join authors_research_papers on research_paper_no = research_papers.paper_id
-// left join authors on authors_research_papers.author_no = authors.author_no
-// where gender = 'f';
-
-// 3
-// select count(author_name),university,avg(h_index)
-// from authors
-// group by university;
-
-// 4
-// select university,count(research_papers.paper_title) As Research_Papers
-// from research_papers
-// left join authors_research_papers on research_paper_no = research_papers.paper_id
-// left join authors on authors_research_papers.author_no = authors.author_no
-// group by university;
+const CONNECTION_CONFIG = {
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  database: 'week2',
+  port: 3306
+};
 
 
-// 5
-// select count(author_name),university, max(h_index), min(h_index)
-// from authors
-// group by university;
+const SELECT_NUMBER_AUTHORS_PAPERS = `
+    SELECT research_papers.paper_title AS research_papers, COUNT(authors.author_name) AS authors_number
+    FROM research_papers
+    LEFT JOIN authors_research_papers ON research_paper_no = research_papers.paper_id
+    LEFT JOIN authors ON authors_research_papers.author_no = authors.author_no
+    GROUP BY paper_title;
+    `;
+
+const SUM_PAPERS_FEMALE = `
+    SELECT COUNT(research_papers.paper_title) AS females_papers
+    FROM research_papers
+    LEFT JOIN authors_research_papers ON research_paper_no = research_papers.paper_id
+    LEFT JOIN authors ON authors_research_papers.author_no = authors.author_no
+    WHERE gender = 'f';
+    `;
+
+const AVERAGE_H_INDEX= `
+    SELECT university,AVG(h_index) AS average
+    FROM authors
+    GROUP BY university;
+    `;
+
+const SUM_PAPERS_UNIVERSITY = `
+    SELECT university,COUNT(research_papers.paper_title) AS research_papers
+    FROM research_papers
+    LEFT JOIN authors_research_papers ON research_paper_no = research_papers.paper_id
+    LEFT JOIN authors on authors_research_papers.author_no = authors.author_no
+    GROUP BY university;
+    `;
+
+const MIN_MAX_H_INDEX = `
+    SELECT university, MAX(h_index), MIN(h_index)
+    FROM authors
+    GROUP BY university;
+    `;
+    
+
+async function seedDatabase() {
+  const connection = mysql.createConnection(CONNECTION_CONFIG);
+  const execQuery = util.promisify(connection.query.bind(connection));
+  try {
+    // call the function that returns promise
+
+    console.log("All research papers and the number of authors that wrote that paper:");
+    console.log(await execQuery(SELECT_NUMBER_AUTHORS_PAPERS));
+
+    console.log("Sum of the research papers published by all female authors:");
+    console.log(await execQuery(SUM_PAPERS_FEMALE));
+
+    console.log("Average of the h-index of all authors per university:");
+    console.log(await execQuery(AVERAGE_H_INDEX));
+
+    console.log("Sum of the research papers of the authors per university:");
+    console.log(await execQuery(SUM_PAPERS_UNIVERSITY));
+
+    console.log("Minimum and maximum of the h-index of all authors per university:");
+    console.log(await execQuery(MIN_MAX_H_INDEX));
+    
+    connection.end();
+  } catch (error) {
+    console.error(error.message);
+    connection.end();
+  } 
+}
+
+seedDatabase();
