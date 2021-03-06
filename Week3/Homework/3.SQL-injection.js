f3884b5a8a361e1a5e75642936e354bda00cffc0
Ahmad Magdy <amagsid@gmail.com>
2021-03-06 13:56:00 +0100

    finished ex. 3/4

--- Week3/Homework/3.SQL-injection.js
+++ Week3/Homework/3.SQL-injection.js
@@ -1,3 +1,18 @@
 "use strict";
 
-//
+//value passed as name and can take advantage of SQL injection: Mars or 1=1 ; SHOW TABLES
+//value passed as code and can take advantage of SQL injection: INJCT or 1=1; DROP DATABASE World
+
+const getPopulation = (Country, name, code, cb) => {
+  conn.query(
+    `SELECT Population FROM ${Country} WHERE Name =` +
+      connection.escape(name) +
+      `and code =` +
+      connection.escape(code),
+    (err, result) => {
+      if (err) cb(err);
+      if (result.length == 0) cb(new Error("Not found"));
+      cb(null, result[0].name);
+    }
+  );
+};
