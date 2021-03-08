a5bf4fcfa85407d4a985eed0baa3b5e6827077e3
MoSaif00 <moh14ab@gmail.com>
2021-03-08 17:46:35 +0100

    finished sql-injection exercise

--- Week3/homework/3-SQL_injection.js
+++ Week3/homework/3-SQL_injection.js
@@ -1,11 +1,53 @@
-function getPopulation(Country, name, code, cb) {
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  database: 'new_world',
+});
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log('Node.js connected to MySQl!');
+});
+
+// 1- Give an example of a value that can be passed as name and code that would take advantage of SQL-injection and (fetch all the records in the database)
+/*
+ the value that  can be passed and take advantage of SQL-injection is the following for example :
+getPopulation(`country`, `India`, `"IND" OR 1=1;show tables;`);
+
+code example : 
+```
+function getPopulation(country, name, code) {
   // assuming that connection to the database is established and stored as conn
-  conn.query(
-    `SELECT Population FROM ${Country} WHERE Name = '${name}' and code = '${code}'`,
+  connection.query(
+    `SELECT Population FROM ${country} WHERE Name = '${name}' AND code = '${code}' `,
     function (err, result) {
-      if (err) cb(err);
-      if (result.length == 0) cb(new Error('Not found'));
-      cb(null, result[0].name);
+      if (err) throw err;
+      console.log('The requested data are ', result);
     }
   );
 }
+getPopulation(`country`, `India`, `"IND" OR 1=1;show tables;`);
+```
+*/
+
+// 2- Rewrite the function so that it is no longer vulnerable to SQL injection
+/* 
+I can Rewrite the function again using prepared statements that will prevent sql-injections attacks like the following example :
+```
+function getPopulation(country, name, code) {
+  // assuming that connection to the database is established and stored as conn
+  connection.query(
+    `SELECT Population FROM ${country} WHERE Name = ? AND code = ?`,
+    [name, code],
+    function (err, result) {
+      if (err) throw err;
+      console.log('The requested data are ', result);
+    }
+  );
+}
+getPopulation('country', 'India', 'IND');
+```
+*/
+
+connection.end();
