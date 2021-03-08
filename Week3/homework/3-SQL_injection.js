0619b09db85cf7798c036ed3bdc0adcf52332a52
MoSaif00 <moh14ab@gmail.com>
2021-03-08 17:06:32 +0100

    sql ingection exercise

---
+++ Week3/homework/3-SQL_injection.js
@@ -0,0 +1,11 @@
+function getPopulation(Country, name, code, cb) {
+  // assuming that connection to the database is established and stored as conn
+  conn.query(
+    `SELECT Population FROM ${Country} WHERE Name = '${name}' and code = '${code}'`,
+    function (err, result) {
+      if (err) cb(err);
+      if (result.length == 0) cb(new Error('Not found'));
+      cb(null, result[0].name);
+    }
+  );
+}
