236ed5ee270a13172e91d658f3d077855f904ca0
MoSaif00 <moh14ab@gmail.com>
2021-03-08 20:50:28 +0100

    fixed sql injection file

--- Week3/homework/3-SQL_injection.js
+++ Week3/homework/3-SQL_injection.js
@@ -1,21 +1,32 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  database: 'new_world',
+});
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log('Node.js connected to MySQl!');
+});
 // 1- Give an example of a value that can be passed as name and code that would take advantage of SQL-injection and (fetch all the records in the database)
 /*
  the value that  can be passed and take advantage of SQL-injection is the following for example :
-getPopulation(`country`, `India`, `"IND" OR 1=1;show tables;`);
+getPopulation(`country`, `India`, `IND  'OR'  1=1 `);
 
 code example : 
 ```
 function getPopulation(country, name, code) {
   // assuming that connection to the database is established and stored as conn
   connection.query(
-    `SELECT Population FROM ${country} WHERE Name = '${name}' AND code = '${code}' `,
+    `SELECT Population FROM ${country} WHERE Name = '${name}' AND code = '${code}; ' `,
     function (err, result) {
       if (err) throw err;
       console.log('The requested data are ', result);
     }
   );
 }
-getPopulation(`country`, `India`, `"IND" OR 1=1;show tables;`);
+getPopulation(`country`, `India`, `IND  'OR'  1=1 `);
 ```
 */
 
@@ -37,3 +48,5 @@ function getPopulation(country, name, code) {
 getPopulation('country', 'India', 'IND');
 ```
 */
+
+connection.end();
