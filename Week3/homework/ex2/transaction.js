1a2fc0f99b6ffed8e5bd70586b643ed37d319a7e
Manal <manalhdkz@gmail.com>
2021-03-09 21:53:13 +0100

    Edit ex2

--- Week3/homework/ex2/transaction.js
+++ Week3/homework/ex2/transaction.js
@@ -20,8 +20,8 @@ async function runQuery() {
     await execQuery(`UPDATE account SET balance = balance - 1000 WHERE account_number = 101`);
     await execQuery(`UPDATE account SET balance = balance + 1000 WHERE account_number = 102`);
 
-    await execQuery(`INSERT INTO account_changes(account_number,amount,changed_date,remark) VALUES(101,${1000},now(),'withdraw Money')`);
-    await execQuery(`INSERT INTO account_changes(account_number,amount,changed_date,remark) VALUES(102,${1000},now(),'Recieved Money')`);
+    await execQuery(`INSERT INTO account_changes(account_number,amount,changed_date,remark) VALUES(101,1000,now(),'withdraw Money')`);
+    await execQuery(`INSERT INTO account_changes(account_number,amount,changed_date,remark) VALUES(102,1000,now(),'Recieved Money')`);
 
     await execQuery('COMMIT');
     connection.end();
