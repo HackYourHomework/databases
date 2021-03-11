cc38cbfb9b07e5caa2c612008377d76aa365eda7
Ahmad Magdy <amagsid@gmail.com>
2021-03-11 11:27:20 +0100

    added the mongoDB connection

--- Week3/Homework/4.mongoDB.js
+++ Week3/Homework/4.mongoDB.js
@@ -1,3 +1,16 @@
-//I included this file for reference.
-//By this point, as shared earlier this week, I lost access to MySQL server and as a result, //couldn't complete ex.4 or refactor the code from the previous exercises.
-//As of this moment, I'm still trying to find a fix for that.
+//By this point in the homework and as ilustrated the past week, I lost access to MySQL server trying to troubleshoot a MySQL error for extracting the data. As a result, couldn't complete ex.4 or refactor the code from the previous exercises.
+//As of this moment, I got the extracted files from my colleagues and I'm working on finishing this exercise starting the MongoDB part.
+
+const MongoClient = require("mongodb").MongoClient;
+const url =
+  "mongodb+srv://hyfuser:hyfpassword@cluster0.rctpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("World").collection("City");
+  console.log(collection);
+  client.close();
+});
