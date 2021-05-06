fcf3757c177dd849c8191dd07f24e1912498550d
Veronika <veroniqe@rambler.ru>
2021-05-06 21:01:49 +0200

    feedback implemented

--- Week1/homework/exercise1.js
+++ Week1/homework/exercise1.js
@@ -6,48 +6,31 @@ const connection = mysql.createConnection({
 });
 connection.connect((err) => {
   if (err) {
-    console.log('Not connected');
     throw err;
   } else {
     console.log('Connected');
   }
 });
 
-const queryDeleteDb = 'DROP DATABASE IF EXISTS meetup;';
-connection.query(queryDeleteDb, (error, results) => {
-  if (error) {
-    throw error;
-  }
-  console.log('DROP DATABASE reply is ', results[0]);
-});
-
-const queryCreateDb = 'CREATE DATABASE meetup;';
-connection.query(queryCreateDb, (error, results) => {
-  if (error) {
-    throw error;
-  }
-  console.log('CREATE DATABASE reply is ', results[0]);
-});
-const querySwitch = 'USE meetup';
-connection.query(querySwitch, (error, results) => {
-  if (error) {
-    console.log('USE error');
-    throw error;
-  }
-  console.log(`USE reply is `, results[0]);
-});
-
 function sendQuery(query) {
   connection.query(query, (error, results) => {
     if (error) {
-      console.log('error');
       throw error;
     }
     console.log(`the reply ${query} is `, results[0]);
   });
 }
+const queryDeleteDb = 'DROP DATABASE IF EXISTS meetup;';
+sendQuery(queryDeleteDb);
+
+const queryCreateDb = 'CREATE DATABASE meetup;';
+sendQuery(queryCreateDb);
+
+const querySwitch = 'USE meetup';
+sendQuery(querySwitch);
+
 const queryTableInvitee =
-  'CREATE TABLE Invitee (invitee_no int, invitee_name varchar(50), invited_by varchar(50))';
+  'CREATE TABLE Invitee (invitee_no int, invitee_name varchar(50), invited_by int)';
 sendQuery(queryTableInvitee);
 
 const queryTableRoom =
@@ -55,20 +38,82 @@ const queryTableRoom =
 sendQuery(queryTableRoom);
 
 const queryTableMeeting =
-  'CREATE TABLE Meeting (meeting_no int, meeting_title varchar(50), starting_time datetime, ending_time datetime,room_no int)';
+  'CREATE TABLE Meeting (meeting_no int, meeting_title varchar(50), starting_time datetime, ending_time datetime, room_no int)';
 sendQuery(queryTableMeeting);
 
-const queryAddRowsToInvitee =
-  'INSERT INTO Invitee VALUES (1,"Jack","Joseph"),(2,"Harry","Jacob"),(3,"Charlie","Oscar"),(4,"James","William"),(5,"Henry","Leo")';
-sendQuery(queryAddRowsToInvitee);
+const rowsForInvitee = [
+  { invitee_no: 1, invitee_name: 'Jack', invited_by: 2 },
+  { invitee_no: 2, invitee_name: 'Harry', invited_by: 5 },
+  { invitee_no: 3, invitee_name: 'Charlie', invited_by: 1 },
+  { invitee_no: 4, invitee_name: 'James', invited_by: 3 },
+  { invitee_no: 5, invitee_name: 'Henry', invited_by: 4 },
+];
+const rowsForRoom = [
+  { room_no: 24, room_name: 'red', floor_number: 2 },
+  { room_no: 38, room_name: 'green', floor_number: 3 },
+  { room_no: 22, room_name: 'blue', floor_number: 2 },
+  { room_no: 31, room_name: 'white', floor_number: 3 },
+  { room_no: 29, room_name: 'black', floor_number: 2 },
+];
+const rowsForMeeting = [
+  {
+    meeting_no: 1,
+    meeting_title: 'first meeting',
+    starting_time: '2021-05-10 15:00:00',
+    ending_time: '2021-05-10 18:00:00',
+    room_no: 24,
+  },
+  {
+    meeting_no: 2,
+    meeting_title: 'second meeting',
+    starting_time: '2021-05-11 15:00:00',
+    ending_time: '2021-05-11 18:00:00',
+    room_no: 29,
+  },
+  {
+    meeting_no: 3,
+    meeting_title: 'third meeting',
+    starting_time: '2021-05-12 15:00:00',
+    ending_time: '2021-05-12 18:00:00',
+    room_no: 31,
+  },
+  {
+    meeting_no: 4,
+    meeting_title: 'forth meeting',
+    starting_time: '2021-05-13 15:00:00',
+    ending_time: '2021-05-13 18:00:00',
+    room_no: 38,
+  },
+  {
+    meeting_no: 5,
+    meeting_title: 'fifth meeting',
+    starting_time: '2021-05-14 15:00:00',
+    ending_time: '2021-05-14 18:00:00',
+    room_no: 22,
+  },
+];
 
-const queryAddRowsToRoom =
-  'INSERT INTO Room VALUES (24,"red",2),(38,"green",3),(22,"blue",2),(31,"white",3),(29,"black",2)';
-sendQuery(queryAddRowsToRoom);
-
-const queryAddRowsToMeeting =
-  'INSERT INTO Meeting VALUES (1, "first meeting", "2021-05-10 15:00:00", "2021-05-10 18:00:00",24), (2, "second meeting", "2021-05-11 15:00:00", "2021-05-11 18:00:00",29), (3, "third meeting", "2021-05-12 15:00:00", "2021-05-12 18:00:00",31),(4, "forth meeting", "2021-05-13 15:00:00", "2021-05-13 18:00:00",38),(5, "fifth meeting", "2021-05-14 15:00:00", "2021-05-14 18:00:00",22)';
-sendQuery(queryAddRowsToMeeting);
+function addRows(arr, table) {
+  arr.forEach((row) => {
+    const objToArr = Object.entries(row);
+    let arrToString = '';
+    for (i = 0; i < objToArr.length; i++) {
+      let value = objToArr[i][1];
+      const key = objToArr[i][0];
+      if (typeof value === 'string') {
+        value = `"${value}"`;
+      }
+      if (i === objToArr.length - 1) {
+        arrToString = arrToString + `${key}=${value}`;
+      } else arrToString = arrToString + `${key}=${value}, `;
+    }
+    const query = `INSERT INTO ${table} SET ${arrToString}`;
+    sendQuery(query);
+  });
+}
+addRows(rowsForInvitee, 'Invitee');
+addRows(rowsForRoom, 'Room');
+addRows(rowsForMeeting, 'Meeting');
 
 connection.end((error) => {
   if (error) {
--- Week1/homework/exercise2.js
+++ Week1/homework/exercise2.js
@@ -7,112 +7,59 @@ const connection = mysql.createConnection({
 });
 connection.connect((err) => {
   if (err) {
-    console.log('Not connected to database');
     throw err;
   } else {
     console.log('Connected to database');
   }
 });
+function sendQuery(query) {
+  connection.query(query, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    console.log(`the reply ${query} is `, results);
+  });
+}
 //1. What are the names of countries with population greater than 8 million
 const query1 = 'SELECT Name FROM country WHERE Population > 8000000';
-connection.query(query1, (error, results, fields) => {
-  if (error) {
-    console.log('error with query1');
-    throw error;
-  }
-  console.log(`the reply "${query1}" is `, results);
-});
+sendQuery(query1);
 
 //2. What are the names of countries that have “land” in their names?
 const query2 = `SELECT Name FROM country WHERE Name LIKE '%land%'`;
-connection.query(query2, (error, results, fields) => {
-  if (error) {
-    console.log('error with query2');
-    throw error;
-  }
-  console.log(`the reply "${query2}" is `, results);
-});
+sendQuery(query2);
 
 //3. What are the names of the cities with population in between 500,000 and 1 million?
 const query3 =
   'SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000';
-connection.query(query3, (error, results, fields) => {
-  if (error) {
-    console.log('error with query3');
-    throw error;
-  }
-  console.log(`the reply "${query3}" is `, results);
-});
+sendQuery(query3);
 
 //4. What's the name of all the countries on the continent ‘Europe’?
 const query4 = `SELECT Name FROM country WHERE Continent = 'Europe'`;
-connection.query(query4, (error, results, fields) => {
-  if (error) {
-    console.log('error with query4');
-    throw error;
-  }
-  console.log(`the reply "${query4}" is `, results);
-});
+sendQuery(query4);
 
 //5. List all the countries in the descending order of their surface areas.
 const query5 = `SELECT Name FROM country ORDER BY SurfaceArea DESC`;
-connection.query(query5, (error, results, fields) => {
-  if (error) {
-    console.log('error with query5');
-    throw error;
-  }
-  console.log(`the reply "${query5}" is `, results);
-});
+sendQuery(query5);
 
 //6. What are the names of all the cities in the Netherlands?
 const query6 = `SELECT Name FROM city WHERE CountryCode = 'NLD'`;
-connection.query(query6, (error, results, fields) => {
-  if (error) {
-    console.log('error with query6');
-    throw error;
-  }
-  console.log(`the reply "${query6}" is `, results);
-});
+sendQuery(query6);
 
 //7. What is the population of Rotterdam?
 const query7 = `SELECT Population FROM city WHERE Name = 'Rotterdam'`;
-connection.query(query7, (error, results, fields) => {
-  if (error) {
-    console.log('error with query7');
-    throw error;
-  }
-  console.log(`the reply "${query7}" is `, results[0]);
-});
+sendQuery(query7);
 
 //8. What's the top 10 countries by Surface Area?
 const query8 = `SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10`;
-connection.query(query8, (error, results, fields) => {
-  if (error) {
-    console.log('error with query8');
-    throw error;
-  }
-  console.log(`the reply "${query8}" is `, results);
-});
+sendQuery(query8);
 
 //9. What's the top 10 most populated cities?
 const query9 = `SELECT Name FROM city ORDER BY Population DESC LIMIT 10`;
-connection.query(query9, (error, results, fields) => {
-  if (error) {
-    console.log('error with query9');
-    throw error;
-  }
-  console.log(`the reply "${query9}" is `, results);
-});
+sendQuery(query9);
 
 //10. What is the population number of the world?
 const query10 = `SELECT SUM(Population) AS worldPopulation FROM country`;
-connection.query(query10, (error, results, fields) => {
-  if (error) {
-    console.log('error with query10');
-    throw error;
-  }
-  console.log(`the reply "${query10}" is `, results[0]);
-});
+sendQuery(query10);
 
 connection.end((error) => {
   if (error) {
