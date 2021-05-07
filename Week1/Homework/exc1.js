46edd39d7f6eae82e8b48a9b07fe21ad76ca5fa9
Mido N Nassar <madyan.nassar@gmail.com>
2021-05-08 00:37:03 +0200

    fixing issues based on FeedBack

--- Week1/Homework/exc1.js
+++ Week1/Homework/exc1.js
@@ -10,54 +10,33 @@ connection.connect((err) => {
   console.log("connected");
 });
 
-connection.query('DROP DATABASE IF EXISTS meetup', (err) => {
-    if (err) throw err;
-    console.log('Database is deleted');
-  });
+const errFunc = (err)=> {if (err) throw err;}
 
-  connection.query("CREATE DATABASE meetup", (err) => {
-    if (err) throw err;
-    console.log('Database is created');
-  });
+connection.query('DROP DATABASE IF EXISTS meetup',errFunc);
 
-connection.query('USE meetup', (err) => {
-    if (err) throw err;
-  });
+connection.query("CREATE DATABASE meetup",errFunc);
+
+connection.query('USE meetup',errFunc);
 
 
-  connection.query('CREATE TABLE Invitee ( invitee_no int , invitee_name varchar(25), invited_by varchar(25))',
-(err) => {
-  if (err) throw err;
-});
+connection.query('CREATE TABLE Invitee ( invitee_no int , invitee_name varchar(25), invited_by varchar(25))',
+errFunc);
 
 
-connection.query('CREATE TABLE Room ( room_no int, room_name varchar(25), floor_number int);',
-(err) => {
-    if (err) throw err;
-});
+connection.query('CREATE TABLE Room ( room_no int, room_name varchar(25), floor_number int)',errFunc);
 
 
-connection.query('CREATE TABLE Meeting ( meeting_no int, meeting_title varchar(40), starting_time datetime, ending_time datetime, room_no int);',
-(err) => {
-    if (err) throw err;
-});
+connection.query('CREATE TABLE Meeting ( meeting_no int, meeting_title varchar(40), starting_time datetime, ending_time datetime, room_no int)',errFunc);
 
-connection.query('INSERT INTO Invitee VALUES (1, "Madyan", "Admin"), (2, "Nour", "Admin"), (3, "Yousef", "Admin"), (4, "Nassar", "Admin"), (5, "Hussin", "Admin");',
-(err) => {
-    if (err) throw err;
-});
+connection.query('INSERT INTO Invitee VALUES (1, "Madyan", "Admin"), (2, "Nour", "Admin"), (3, "Yousef", "Admin"), (4, "Nassar", "Admin"), (5, "Hussin", "Admin")',errFunc);
 
-connection.query('INSERT INTO Room VALUES (4, "Room_1", 1), (2, "Room_2", 2),(3, "Room_3", 3),(1, "Room_4", 4),(5, "Room_5", 5)',
-(err) => {
-    if (err) throw err;
-});
+connection.query('INSERT INTO Room VALUES (4, "Room_1", 1), (2, "Room_2", 2),(3, "Room_3", 3),(1, "Room_4", 4),(5, "Room_5", 5)',errFunc);
 
-connection.query('INSERT INTO Meeting VALUES (1, "Meeting_1", "2021-05-02 8:00:00",	"2021-05-02 9:00:00", 1),(2, "Meeting_2", "2021-05-02 09:30:00", "2021-05-02 10:30:00",	2),(3, "Meeting_3",	"2021-05-02 11:00:00",	"2021-05-02 12:00:00",	3),(4,	"Meeting_4",	"2021-05-02 12:30:00","2021-05-02 13:30:00",4),(5,	"Meeting_5", "2021-05-02 13:30:00",	"2021-05-02 14:30:00 ", 5)',
-(err) => {
-    if (err) throw err;
-});
+connection.query('INSERT INTO Meeting VALUES (1, "Meeting_1", "2021-05-02 8:00:00",	"2021-05-02 9:00:00", 1),(2, "Meeting_2", "2021-05-02 09:30:00", "2021-05-02 10:30:00",	2),(3, "Meeting_3",	"2021-05-02 11:00:00",	"2021-05-02 12:00:00",	3),(4,	"Meeting_4",	"2021-05-02 12:30:00","2021-05-02 13:30:00",4),(5,	"Meeting_5", "2021-05-02 13:30:00",	"2021-05-02 14:30:00 ", 5)',errFunc);
 
-connection.query('SELECT * FROM Invitee , Room , Meeting  ;',
+
+
+connection.query('SELECT * FROM Invitee , Room , Meeting',
 (err, result) => {
   if (err) throw err;
   console.table(result);
--- Week1/Homework/exc2.js
+++ Week1/Homework/exc2.js
@@ -11,65 +11,29 @@ connection.query("USE world", (error) => {
     if (error) throw error;
   });
 
-  connection.query('SELECT Name FROM country WHERE Population > 8000000;',
-  (err, result) => {
+  const queryFunc = (err,result)=>{
     if (err) throw err;
     console.log(result);
-});
+  }
 
-connection.query("SELECT name FROM country WHERE name LIKE '%land%';",
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query('SELECT Name FROM country WHERE Population > 8000000',queryFunc);
 
-connection.query('SELECT name FROM city WHERE population BETWEEN 500000 AND 1000000;',
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query("SELECT name FROM country WHERE name LIKE '%land%'",queryFunc);
 
-connection.query('SELECT name FROM country WHERE continent = "Europe";',
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query('SELECT name FROM city WHERE population BETWEEN 500000 AND 1000000',queryFunc);
 
-connection.query('SELECT name FROM country ORDER BY SurfaceArea DESC;',
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query('SELECT name FROM country WHERE continent = "Europe"',queryFunc);
 
-connection.query('SELECT name FROM city WHERE CountryCode = "NLD";',
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query('SELECT name FROM country ORDER BY SurfaceArea DESC',queryFunc);
 
-connection.query('SELECT population FROM city WHERE name = "Rotterdam";',
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query('SELECT name FROM city WHERE CountryCode = "NLD"',queryFunc);
 
-connection.query('SELECT name FROM country ORDER BY SurfaceArea DESC LIMIT 10;',
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query('SELECT population FROM city WHERE name = "Rotterdam"',queryFunc);
 
-connection.query('SELECT name FROM city ORDER BY population DESC LIMIT 10;',
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query('SELECT name FROM country ORDER BY SurfaceArea DESC LIMIT 10',queryFunc);
 
-connection.query('SELECT SUM(population) FROM country',
-(err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+connection.query('SELECT name FROM city ORDER BY population DESC LIMIT 10',queryFunc);
 
+connection.query('SELECT SUM(population) AS WorldPopulation FROM country',queryFunc);
 
 connection.end();
