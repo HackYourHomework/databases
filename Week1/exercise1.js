ad784b2ad62cfa9577e62d2430fb645571f71052
osamaalpha <osamamohamedupisdown@gmail.com>
2021-05-11 17:30:15 +0300

    get the queries out of the connection and ending connection fix variables names

--- Week1/exercise1.js
+++ Week1/exercise1.js
@@ -1,63 +1,57 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Alphagama6',
-    database:"meetup"
-  });
-  con.connect(function(err) {
-    if (err) throw err;
-    con.query("CREATE DATABASE meetup", function (err, result) {
-        if (err) throw err;
-      });
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "meetup",
+});
+con.connect(function (err) {
+  if (err) throw err;
+});
+con.query("CREATE DATABASE IF NOT EXISTS meetup", function (err, result) {
+  if (err) throw err;
+});
 
-    const sql = "CREATE TABLE Invitee (invitee_no INT(11), invitee_name VARCHAR(255),invited_by VARCHAR(255))";
+const createTableInvitee =
+  "CREATE TABLE IF NOT EXISTS Invitee (invitee_no INT, invitee_name VARCHAR(50),invited_by VARCHAR(50))";
 
-  con.query(sql,(err,result)=>{
-      if(err) throw err
-  })
-  const sql2 = "CREATE TABLE Room (room_no INT(11) PRIMARY KEY, room_name VARCHAR(255),floor_number INT(11))";
+con.query(createTableInvitee, (err, result) => {
+  if (err) throw err;
+});
+const createTableRoom =
+  "CREATE TABLE IF NOT EXISTS Room (room_no INT PRIMARY KEY, room_name VARCHAR(50),floor_number INT)";
 
-  con.query(sql2,(err,result)=>{
-      if(err) throw err
+con.query(createTableRoom, (err, result) => {
+  if (err) throw err;
+});
+const createTableMeeting =
+  "CREATE TABLE IF NOT EXISTS Meeting (meeting_no INT, meeting_title VARCHAR(50),starting_time TIME ," +
+  "ending_time TIME, room_no INT,  FOREIGN KEY (room_no) REFERENCES Room(room_no))";
+con.query(createTableMeeting, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+});
 
-  })
-  const sql4 ="ALTER TABLE Room ADD PRIMARY KEY(room_no);"
-  con.query(sql4,(err,result)=>{
-    if(err) throw err
-    console.log(result)
-})
-  const sql3 = "CREATE TABLE Meeting (meeting_no INT(11), meeting_title VARCHAR(255),starting_time TIME ,"
-   "ending_time TIME, room_no INT(11),  FOREIGN KEY (room_no) REFERENCES Room(room_no))";
+const sqlInsertTableInvitee =
+  "INSERT INTO Invitee (invitee_no,invitee_name,invited_by)" +
+  " VALUES (1,'osama','Eman'), (2,'mohamed','Rob'),(3,'mahmoud','olga'),(4,'ismael','jack'),(5,'ahmed','john') ";
+con.query(sqlInsertTableInvitee, function (err, result) {
+  if (err) throw err;
+});
 
-  con.query(sql3,(err,result)=>{
-      if(err) throw err
-      console.log(result)
-  })
-       
-   const sqlInsertTableInvitee = "INSERT INTO Invitee (invitee_no,invitee_name,invited_by)"+
-   " VALUES (1,'osama','Eman'), (2,'mohamed','Rob'),(3,'mahmoud','olga'),(4,'ismael','jack'),(5,'ahmed','john') " 
-   con.query(sqlInsertTableInvitee, function (err, result) {
-    if (err) throw err;
-  });
+const sqlInsertTableRoom =
+  "INSERT INTO Room (room_no,room_name,floor_number)" +
+  "VALUES (1,'osama',1), (2,'mohamed',2),(3,'mahmoud',3),(4,'ismael',4),(5,'ahmed',5) ";
+con.query(sqlInsertTableRoom, function (err, result) {
+  if (err) throw err;
+});
 
-  const sqlInsertTableRoom = "INSERT INTO Room (room_no,room_name,floor_number)"+
-  "VALUES (1,'osama',1), (2,'mohamed',2),(3,'mahmoud',3),(4,'ismael',4),(5,'ahmed',5) " 
-  con.query(sqlInsertTableRoom, function (err, result) {
-   if (err) throw err;
- });
-
- const sqlInsertTableMeeting = "INSERT INTO Meeting (meeting_no,meeting_title,starting_time,ending_time,room_no)"+
- "VALUES (1,'chat','05:02','06:00',1), (2,'eating','06:02','07:00',2),(3,'drink','07:02','08:00',3),"+
- "(4,'standup','09:02','10:00',4),(5,'business','11:15','12:00',5) " ;
-  con.query(sqlInsertTableMeeting, function (err, result) {
-   if (err) throw err;
- });
-
-  });
-
-  
-    
-
-  
+const sqlInsertTableMeeting =
+  "INSERT INTO Meeting (meeting_no,meeting_title,starting_time,ending_time,room_no)" +
+  "VALUES (1,'chat','05:02','06:00',1), (2,'eating','06:02','07:00',2),(3,'drink','07:02','08:00',3)," +
+  "(4,'standup','09:02','10:00',4),(5,'business','11:15','12:00',5) ";
+con.query(sqlInsertTableMeeting, function (err, result) {
+  if (err) throw err;
+});
+con.end();
--- Week1/exercise2.js
+++ Week1/exercise2.js
@@ -1,66 +1,90 @@
-const fs = require('fs');
-const mysql = require('mysql')
+const fs = require("fs");
+const mysql = require("mysql");
 
 // Read the SQL file
-const dataSql = fs.readFileSync('./world.sql').toString();
+const dataSql = fs.readFileSync("./world.sql").toString();
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Alphagama6",
-    database: "world",
-    multipleStatements: true
-  });
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "world",
+  multipleStatements: true,
+});
 
-
-con.connect(function(err) {
+con.connect(function (err) {
+  if (err) throw err;
+});
+con.query(dataSql, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+});
+con.query(
+  "SELECT Name FROM country WHERE Population > 8000000",
+  (err, result) => {
     if (err) throw err;
-    con.query(dataSql, (err, result) =>{
-      if (err) throw err;
-      console.log(result);
-    });
-    con.query("SELECT Name FROM country WHERE Population > 8000000",(err,result)=>{
-        if(err) throw err
-        console.log(result)
-    })
-    con.query("SELECT Name FROM country WHERE Name LIKE '%land%'",(err,result)=>{
-        if(err) throw err
-        console.log(result)
-    })
-    con.query("SELECT Name FROM city WHERE Population BETWEEN '500,000' AND '1000000' ",(err,result)=>{
-        if(err) throw err
-        console.log(result)
-    })
-    con.query("SELECT Name FROM country WHERE continent= 'Europe'",(err,result)=>{
-        if(err) throw err
-        console.log(result)
-    })
-    con.query("SELECT Name FROM country ORDER BY SurfaceArea DESC ",(err,result)=>{
-        if(err) throw err 
-        console.log(result)
-    })
-    con.query("SELECt city.Name FROM city INNER JOIN country on city.CountryCode=country.Code "+
-    "AND country.Name='Netherlands'",(err,result)=>{
-        if(err) throw err
-        console.log(result)
-    })
-    con.query("SELECT Population FROM city WHERE Name='Rotterdam'",(err,result)=>{
-        if(err) throw err
-        console.log(result)
-    })
-    con.query("SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10 ",(err,result)=>{
-        if(err) throw err 
-        console.log(result)
-    })
-    con.query("SELECT Name FROM city ORDER BY Population DESC LIMIT 10 ",(err,result)=>{
-        if(err) throw err 
-        console.log(result)
-    })
-    con.query("SELECT SUM(Population) FROM Country",(err,result)=>{
-        if(err) throw err
-        console.log(result)
-    })
-
-  });
-
-
+    console.log(result);
+  }
+);
+con.query(
+  "SELECT Name FROM country WHERE Name LIKE '%land%'",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+con.query(
+  "SELECT Name FROM city WHERE Population BETWEEN '500000' AND '1000000' ",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+con.query(
+  "SELECT Name FROM country WHERE continent= 'Europe'",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+con.query(
+  "SELECT Name FROM country ORDER BY SurfaceArea DESC ",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+con.query(
+  "SELECt city.Name FROM city INNER JOIN country on city.CountryCode=country.Code " +
+    "AND country.Name='Netherlands'",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+con.query(
+  "SELECT Population FROM city WHERE Name='Rotterdam'",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+con.query(
+  "SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10 ",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+con.query(
+  "SELECT Name FROM city ORDER BY Population DESC LIMIT 10 ",
+  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  }
+);
+con.query("SELECT SUM(Population) FROM Country", (err, result) => {
+  if (err) throw err;
+  console.log(result);
+});
+con.end();
