0d9d5c333570ee7d7dc8b7c5e30743f31fa12c12
osamaalpha <osamamohamedupisdown@gmail.com>
2021-05-12 20:46:01 +0300

    fix string to number and make function for usability

--- Week1/exercise2.js
+++ Week1/exercise2.js
@@ -15,76 +15,25 @@ var con = mysql.createConnection({
 con.connect(function (err) {
   if (err) throw err;
 });
-con.query(dataSql, (err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
-con.query(
-  "SELECT Name FROM country WHERE Population > 8000000",
-  (err, result) => {
+const executeQuery = (query) => {
+  con.query(query, (err, result) => {
     if (err) throw err;
     console.log(result);
-  }
+  });
+};
+executeQuery("SELECT Name FROM country WHERE Population > 8000000");
+executeQuery(
+  "SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000 "
 );
-con.query(
-  "SELECT Name FROM country WHERE Name LIKE '%land%'",
-  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  }
-);
-con.query(
-  "SELECT Name FROM city WHERE Population BETWEEN '500000' AND '1000000' ",
-  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  }
-);
-con.query(
-  "SELECT Name FROM country WHERE continent= 'Europe'",
-  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  }
-);
-con.query(
-  "SELECT Name FROM country ORDER BY SurfaceArea DESC ",
-  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  }
-);
-con.query(
+executeQuery("SELECT Name FROM country WHERE continent= 'Europe'");
+executeQuery("SELECT Name FROM country ORDER BY SurfaceArea DESC ");
+executeQuery(
   "SELECt city.Name FROM city INNER JOIN country on city.CountryCode=country.Code " +
-    "AND country.Name='Netherlands'",
-  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  }
+    "AND country.Name='Netherlands'"
 );
-con.query(
-  "SELECT Population FROM city WHERE Name='Rotterdam'",
-  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  }
-);
-con.query(
-  "SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10 ",
-  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  }
-);
-con.query(
-  "SELECT Name FROM city ORDER BY Population DESC LIMIT 10 ",
-  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  }
-);
-con.query("SELECT SUM(Population) FROM Country", (err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
+executeQuery("SELECT Population FROM city WHERE Name='Rotterdam'");
+executeQuery("SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10 ");
+executeQuery("SELECT Name FROM city ORDER BY Population DESC LIMIT 10 ");
+executeQuery("SELECT SUM(Population) FROM Country");
+
 con.end();
